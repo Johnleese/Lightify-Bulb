697e1917f12c1d5d8ecaf1ef7ac12385fd6dd641
Johnleese <Johnleese@gmail.com>
2016-08-02 23:04:46 -0500

    Update Virtual_Lightify_Bulb_v2.groovy

--- Virtual_Lightify_Bulb_v2.groovy
+++ Virtual_Lightify_Bulb_v2.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  2016-01-26 - Updated virtual device to use official Color Temperature Capability instead of custom commands
+ *  2016-01-26 - Updated virtual device to use official Color Temperature Capability instead of custom commands 
  * 
  */
  
